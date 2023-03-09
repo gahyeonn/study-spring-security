f807fae7389f05de49ba0433b600962979c56820
GaHyeon <parka0017@gmail.com>
2023-03-09 15:20:19 +0900

    formLogin 방식으로 인증 방식 변경

---
+++ ssia-ch5-ex4/src/main/java/com/gahyeonn/ssiach5ex4/config/ProjectConfig.java
@@ -0,0 +1,15 @@
+package com.gahyeonn.ssiach5ex4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //양식 기반 로그인으로 인증 방식 변경
+        http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
--- ssia-ch5-ex4/src/main/java/com/gahyeonn/ssiach5ex4/controllers/HelloController.java
+++ ssia-ch5-ex4/src/main/java/com/gahyeonn/ssiach5ex4/controllers/HelloController.java
@@ -3,6 +3,7 @@ package com.gahyeonn.ssiach5ex4.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//html 뷰 반환 위함
 @Controller
 public class HelloController {
 
