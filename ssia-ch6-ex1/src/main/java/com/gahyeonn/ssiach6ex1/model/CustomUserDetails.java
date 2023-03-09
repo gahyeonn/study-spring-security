1257b5d2604ee45262652fd665d8f8e4a9836375
GaHyeon <parka0017@gmail.com>
2023-03-09 18:32:52 +0900

    CustomUserDetails 구현

---
+++ ssia-ch6-ex1/src/main/java/com/gahyeonn/ssiach6ex1/model/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.gahyeonn.ssiach6ex1.model;
+
+import com.gahyeonn.ssiach6ex1.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public final User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities().stream()
+                .map(a -> new SimpleGrantedAuthority( //디비에서 각 사용자에 대해 발견된 각 권한 이름을 SimpleGrantedAuthority로 매핑
+                        a.getName()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ ssia-ch6-ex1/src/main/java/com/gahyeonn/ssiach6ex1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gahyeonn.ssiach6ex1.repository;
+
+import com.gahyeonn.ssiach6ex1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findUserByUsername(Integer integer);
+}
