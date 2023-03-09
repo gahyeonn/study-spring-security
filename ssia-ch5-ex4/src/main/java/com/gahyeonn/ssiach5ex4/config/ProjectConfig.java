c153ecc574aaa009e33ba63588e2584e5d3470ae
GaHyeon <parka0017@gmail.com>
2023-03-09 15:47:52 +0900

    Custom Handler 등록과 http basic 인증 추가

--- ssia-ch5-ex4/src/main/java/com/gahyeonn/ssiach5ex4/config/ProjectConfig.java
+++ ssia-ch5-ex4/src/main/java/com/gahyeonn/ssiach5ex4/config/ProjectConfig.java
@@ -1,15 +1,30 @@
 package com.gahyeonn.ssiach5ex4.config;
 
+import com.gahyeonn.ssiach5ex4.handler.CustomAuthenticationFailureHandler;
+import com.gahyeonn.ssiach5ex4.handler.CustomAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private CustomAuthenticationFailureHandler authenticationFailureHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //양식 기반 로그인으로 인증 방식 변경
-        http.formLogin().defaultSuccessUrl("/home", true);
+        http.formLogin()
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+            .and()
+                    .httpBasic();
+
         http.authorizeRequests().anyRequest().authenticated();
     }
 }
