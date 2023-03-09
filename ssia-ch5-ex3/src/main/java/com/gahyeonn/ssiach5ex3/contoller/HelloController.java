365f5f7af2f80e311decd6f52579279dbb6ceedc
GaHyeon <parka0017@gmail.com>
2023-03-09 14:03:37 +0900

    5-3 setting

---
+++ ssia-ch5-ex3/src/main/java/com/gahyeonn/ssiach5ex3/contoller/HelloController.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach5ex3.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
