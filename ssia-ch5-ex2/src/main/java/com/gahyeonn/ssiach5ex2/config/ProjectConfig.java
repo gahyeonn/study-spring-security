1124dafba28403d61b8f69d37484a52938763078
GaHyeon <parka0017@gmail.com>
2023-03-09 12:15:30 +0900

    InitializingBean을 이용해서 SecurityContextHolder 모드 설정

--- ssia-ch5-ex2/src/main/java/com/gahyeonn/ssiach5ex2/config/ProjectConfig.java
+++ ssia-ch5-ex2/src/main/java/com/gahyeonn/ssiach5ex2/config/ProjectConfig.java
@@ -1,4 +1,21 @@
 package com.gahyeonn.ssiach5ex2.config;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+@EnableAsync
 public class ProjectConfig {
+
+    //비동기 사용 시 메서드가 보안 컨텍스트를 상속하지 않는 다른 스레드에서 실행되기 때문에 기존 MODE_THREADLOCAL 전략으로는 보안 컨텍스트 사용 X
+    //InitializingBean을 이용해서 SecurityContextHolder 모드 설정
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(
+                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL
+        );
+    }
 }
--- ssia-ch5-ex2/src/main/java/com/gahyeonn/ssiach5ex2/controller/HelloController.java
+++ ssia-ch5-ex2/src/main/java/com/gahyeonn/ssiach5ex2/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.gahyeonn.ssiach5ex2.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,5 +12,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(Authentication a) {
         return "Hello, " + a.getName() + "!";
+    }
+
+    @GetMapping("/bye")
+    @Async //메서드가 별도의 스레드에서 실행 => 비동기
+    public void goodbye() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
     }
 }
