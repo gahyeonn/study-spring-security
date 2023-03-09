6469898925ff304cc1d04fd29b5872debff84849
GaHyeon <parka0017@gmail.com>
2023-03-09 14:17:49 +0900

    인증 실패를 위한 응답의 영역 이름 구성

---
+++ ssia-ch5-ex3/src/main/java/com/gahyeonn/ssiach5ex3/config/ProjectConfig.java
@@ -0,0 +1,19 @@
+package com.gahyeonn.ssiach5ex3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //인증 방식과 관련한 일부 구성(영역 이름)을 설정할 수 있음
+        //영역(realm)은 특정 인증 방식을 이용하는 보호 공간
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+        });
+
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
