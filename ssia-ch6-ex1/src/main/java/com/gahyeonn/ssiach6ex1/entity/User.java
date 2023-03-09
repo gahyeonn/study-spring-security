facf79e671c71bdea253d77acded5288d5d0dde2
GaHyeon <parka0017@gmail.com>
2023-03-09 18:23:58 +0900

    사용자 관련 entity 정의

---
+++ ssia-ch6-ex1/src/main/java/com/gahyeonn/ssiach6ex1/entity/User.java
@@ -0,0 +1,29 @@
+package com.gahyeonn.ssiach6ex1.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private EncryptionAlgorithm algorithm;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<Authority> authorities;
+
+    private enum EncryptionAlgorithm {
+        BCRYPT, SCRYPT
+    }
+}
