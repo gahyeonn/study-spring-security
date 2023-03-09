a5a68494ef78be4ee6b2b50e203c39c847ea6a10
GaHyeon <parka0017@gmail.com>
2023-03-09 13:40:44 +0900

    DelegatingSecurityContextCallable 유틸리티를 적용해 작업 자체를 이용하여 보안 컨텍스트 전파

--- ssia-ch5-ex2/src/main/java/com/gahyeonn/ssiach5ex2/controller/HelloController.java
+++ ssia-ch5-ex2/src/main/java/com/gahyeonn/ssiach5ex2/controller/HelloController.java
@@ -1,11 +1,17 @@
 package com.gahyeonn.ssiach5ex2.controller;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -19,5 +25,23 @@ public class HelloController {
     public void goodbye() {
         SecurityContext context = SecurityContextHolder.getContext();
         String username = context.getAuthentication().getName();
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+
+        //Callable 객체를 정의해 별도의 스레드에서 작업 실행
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        try{
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
     }
 }
