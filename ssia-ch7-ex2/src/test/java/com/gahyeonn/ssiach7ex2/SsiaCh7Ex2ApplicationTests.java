9a95fbf0e12c8fa6777fb161a11e3fd8beeb9a71
GaHyeon <parka0017@gmail.com>
2023-03-24 14:21:49 +0900

    7-2 setting

---
+++ ssia-ch7-ex2/src/test/java/com/gahyeonn/ssiach7ex2/SsiaCh7Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach7ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh7Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
