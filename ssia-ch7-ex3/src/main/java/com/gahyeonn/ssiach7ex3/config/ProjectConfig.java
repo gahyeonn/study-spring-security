1eacd6e97c5142ce7ea4ec8aff475a799d9592cb
GaHyeon <parka0017@gmail.com>
2023-03-24 14:42:24 +0900

    authorities()를 이용한 역할 설정

---
+++ ssia-ch7-ex3/src/main/java/com/gahyeonn/ssiach7ex3/config/ProjectConfig.java
@@ -0,0 +1,48 @@
+package com.gahyeonn.ssiach7ex3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+
+        var user1 = User.withUsername("john")
+                .password("12345")
+                .authorities("ROLE_ADMIN") //역할 선언 시에 'ROLE_' 접두사 필요
+                .build();
+
+        var user2 = User.withUsername("jane")
+                .password("12345")
+                .authorities("ROLE_MANAGER")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .hasRole("ADMIN"); //역할 사용 시에는 'ROLE_' 안붙여도 됨
+    }
+}
