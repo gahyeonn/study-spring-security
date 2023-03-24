5f7bf36d8ba8edc47c15f7bf48d473a0cd40208a
GaHyeon <parka0017@gmail.com>
2023-03-24 16:09:36 +0900

    8-1 setting

---
+++ ssia-ch8-ex1/src/main/java/com/gahyeonn/ssiach8ex1/SsiaCh8Ex1Application.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach8ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh8Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh8Ex1Application.class, args);
+	}
+
+}
