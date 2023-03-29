e8aeb716177d2ec39c6e97b1c6ed055446e5af98
GaHyeon <parka0017@gmail.com>
2023-03-29 23:21:17 +0900

    9-3 setting

---
+++ ssia-ch9-ex3/src/test/java/com/gahyeonn/ssiach9ex3/SsiaCh9Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach9ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh9Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
