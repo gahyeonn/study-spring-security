c68379e8c00b756e26f78403f0e22dcced28e3a3
GaHyeon <parka0017@gmail.com>
2023-03-29 22:37:22 +0900

    filter 디버깅 로그 출력 애너테이션 추가

--- ssia-ch9-ex1/src/main/java/com/gahyeonn/ssiach9ex1/config/ProjectConfig.java
+++ ssia-ch9-ex1/src/main/java/com/gahyeonn/ssiach9ex1/config/ProjectConfig.java
@@ -3,9 +3,11 @@ package com.gahyeonn.ssiach9ex1.config;
 import com.gahyeonn.ssiach9ex1.filter.RequestValidationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+@EnableWebSecurity(debug = true)
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
