bae40bf457cc67f7833eb7e531228610668509b5
GaHyeon <parka0017@gmail.com>
2023-03-29 23:14:30 +0900

    기본 UserDetailsService 구성 비활성화

--- ssia-ch9-ex2/src/main/java/com/gahyeonn/ssiach9ex2/SsiaCh9Ex2Application.java
+++ ssia-ch9-ex2/src/main/java/com/gahyeonn/ssiach9ex2/SsiaCh9Ex2Application.java
@@ -2,8 +2,10 @@ package com.gahyeonn.ssiach9ex2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+//현재 사용자를 사용하고 있지 않기 때문에 기본 UserDetailsService 구성 비활성화
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class SsiaCh9Ex2Application {
 
 	public static void main(String[] args) {
