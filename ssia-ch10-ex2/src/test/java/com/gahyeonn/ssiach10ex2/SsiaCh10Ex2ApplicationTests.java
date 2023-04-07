9fa66ad529f57ec87a40effd187417483de1bca7
GaHyeon <parka0017@gmail.com>
2023-04-07 15:55:22 +0900

    10-2 setting

---
+++ ssia-ch10-ex2/src/test/java/com/gahyeonn/ssiach10ex2/SsiaCh10Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach10ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh10Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
