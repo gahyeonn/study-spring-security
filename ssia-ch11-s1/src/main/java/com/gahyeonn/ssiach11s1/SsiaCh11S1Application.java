5579f9f3602ec06d3222266f4c8906e65a67f5b8
GaHyeon <parka0017@gmail.com>
2023-04-14 14:54:17 +0900

    11 인증 서버 구현 setting

---
+++ ssia-ch11-s1/src/main/java/com/gahyeonn/ssiach11s1/SsiaCh11S1Application.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach11s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh11S1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh11S1Application.class, args);
+	}
+
+}
