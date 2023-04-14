f5e731f20c99357f457aef38e4080cad35615c48
GaHyeon <parka0017@gmail.com>
2023-04-14 16:20:26 +0900

    11 비즈니스 논리 서버 구현 setting

---
+++ ssia-ch11-s2/src/main/java/com/gahyeonn/ssiach11s2/SsiaCh11S2Application.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach11s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh11S2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh11S2Application.class, args);
+	}
+
+}
---
+++ ssia-ch11-s2/src/test/java/com/gahyeonn/ssiach11s2/SsiaCh11S2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gahyeonn.ssiach11s2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh11S2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
