4961ce7619ec1bc741d2d828e590a848b0c338bd
GaHyeon <parka0017@gmail.com>
2023-04-15 23:58:48 +0900

    authenticationProvider 구현

---
+++ ssia-ch11-s2/src/main/java/com/gahyeonn/ssiach11s2/authentication/provider/UsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,32 @@
+package com.gahyeonn.ssiach11s2.authentication.provider;
+
+import com.gahyeonn.ssiach11s2.authentication.UsernamePasswordAuthentication;
+import com.gahyeonn.ssiach11s2.authentication.proxy.AuthenticationServerProxy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+    private final AuthenticationServerProxy proxy;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        //proxy로 인증 서버를 호출하고 OTP를 보냄
+        proxy.sendAuth(username, password);
+
+        return new UsernamePasswordAuthenticationToken(username, password);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthentication.class.isAssignableFrom(authentication);
+    }
+}
