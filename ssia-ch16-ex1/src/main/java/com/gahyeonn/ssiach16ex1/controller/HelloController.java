a188d595e1eb967525752aae2564169ea6f074ce
GaHyeon <parka0017@gmail.com>
2023-05-30 16:23:33 +0900

    메서드 매개 변수를 이용한 사전 권한 부여 설정 구현

--- ssia-ch16-ex1/src/main/java/com/gahyeonn/ssiach16ex1/controller/HelloController.java
+++ ssia-ch16-ex1/src/main/java/com/gahyeonn/ssiach16ex1/controller/HelloController.java
@@ -3,7 +3,10 @@ package com.gahyeonn.ssiach16ex1.controller;
 import com.gahyeonn.ssiach16ex1.service.NameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -17,5 +20,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, " + nameService.getName(); //사전 권한 부여 규칙 적용한 메서드 호출
+    }
+
+    @GetMapping("/secret/names/{name}")
+    public List<String> names(@PathVariable String name) {
+        return nameService.getSecretNames(name);
     }
 }
--- ssia-ch16-ex1/src/main/java/com/gahyeonn/ssiach16ex1/service/NameService.java
+++ ssia-ch16-ex1/src/main/java/com/gahyeonn/ssiach16ex1/service/NameService.java
@@ -3,11 +3,22 @@ package com.gahyeonn.ssiach16ex1.service;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class NameService {
+
+    private Map<String, List<String>> secretNames = Map.of("natalie", List.of("Energico", "Perfecto"),
+                                                            "emma", List.of("Fantastico"));
 
     @PreAuthorize("hasAuthority('write')") //권한 부여 규칙 정의 - spring expression language로 기술
     public String getName() {
         return "Fantastico";
+    }
+
+    @PreAuthorize("#name == authentication.principal.username")
+    public List<String> getSecretNames(String name) {
+        return secretNames.get(name);
     }
 }
